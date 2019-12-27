8a6ac3cd7b972066c5f7e19aedc7328755f2157f
Loic <l.lefloch.91@gmail.com>
2019-12-27 09:41:50 +0100

    update ci plugin

--- reacticoon-cli-plugin-ci/index.js
+++ reacticoon-cli-plugin-ci/index.js
@@ -1,6 +1,13 @@
 const createReacticoonPlugin = require("create-reacticoon-app/plugin/createReacticoonPlugin");
 const createServerCommand = require("create-reacticoon-app/server/createServerCommand");
 
+const getCurrentProjectStateCommand = createServerCommand(
+  "CI::PROJECT::STATE",
+  "Retrieve project CI state",
+  "./server-commands/current-project-state",
+  {}
+);
+
 const getCiRepositoryInfosCommand = createServerCommand(
   "CI::REPOSITORY::INFOS",
   "Retrieve ci respository data",
@@ -19,5 +26,9 @@ module.exports = createReacticoonPlugin({
   checkup: [],
   generators: [],
   commands: [],
-  serverCommands: [getCiRepositoryInfosCommand, getCiBuildInfosCommand]
+  serverCommands: [
+    getCurrentProjectStateCommand,
+    getCiRepositoryInfosCommand,
+    getCiBuildInfosCommand
+  ]
 });
---
+++ reacticoon-cli-plugin-ci/server-commands/current-project-state.js
@@ -0,0 +1,27 @@
+function getState(pluginApi) {
+  const hasTravisCi = pluginApi.hasProjectFile(".travis.yml");
+  let configuration = null;
+
+  if (hasTravisCi) {
+    rawConfiguration = pluginApi.readProjectFile(".travis.yml");
+    configuration = rawConfiguration
+      ? pluginApi.parseYaml(rawConfiguration)
+      : null;
+  }
+
+  state = {
+    hasCi: hasTravisCi,
+    ciType: hasTravisCi ? "travis" : null,
+    configuration,
+    rawConfiguration
+  };
+
+  return state;
+}
+
+function CommandCurrentProjectState(req, res, pluginApi) {
+  const state = getState(pluginApi);
+  res.json(state);
+}
+
+module.exports = CommandCurrentProjectState;
