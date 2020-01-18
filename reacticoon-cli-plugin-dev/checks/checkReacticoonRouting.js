a0957757fd1e360eef5e16eee11d8253ee8b9e44
Loic <l.lefloch.91@gmail.com>
2020-01-18 19:30:40 +0100

    refactor routing config

--- reacticoon-cli-plugin-dev/checks/checkReacticoonRouting.js
+++ reacticoon-cli-plugin-dev/checks/checkReacticoonRouting.js
@@ -1,41 +1,40 @@
-const { getRoutes } = require('create-reacticoon-app/front')
+const { getRoutes } = require("create-reacticoon-app/front");
 
-const run = (api) => {
-  const routes = getRoutes()
+const run = api => {
+  const routes = getRoutes();
 
   if (!routes) {
     api.error(
       true,
-      `routes are not accessible`
-      `Does the sse server correctly running ?`
-    ) 
-    return
+      `routes are not accessible``Does the sse server correctly running ?`
+    );
+    return;
   }
 
-  const names = {}
-  const path = {}
+  const names = {};
+  const path = {};
   routes.forEach(route => {
     if (names[route.definition.name] === true) {
       api.error(
         false,
-        `Route name '${route.definition.name}' is duplicated`,
-        `Route name '${route.definition.name} is duplicated`
-      ) 
+        `RouteDefinition name '${route.definition.name}' is duplicated`,
+        `RouteDefinition name '${route.definition.name} is duplicated`
+      );
     }
     if (path[route.definition.path] === true) {
       api.error(
         false,
-        `Route path '${route.definition.path}' is duplicated`,
-        `Route path '${route.definition.path}' is duplicated`
-      ) 
+        `RouteDefinition path '${route.definition.path}' is duplicated`,
+        `RouteDefinition path '${route.definition.path}' is duplicated`
+      );
     }
-    names[route.definition.name] = true
-    path[route.definition.path] = true
-  })
-}
+    names[route.definition.name] = true;
+    path[route.definition.path] = true;
+  });
+};
 
 module.exports = {
-  name: 'Routing',
-  description: 'Check that the project routing is correct',
-  run,
-}
+  name: "Routing",
+  description: "Check that the project routing is correct",
+  run
+};
